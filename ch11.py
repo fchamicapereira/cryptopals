1d26ea9bbfedbe4dd138f1dd711f99fa001e878f
fchamicapereira <fchamicapereira@gmail.com@>
2018-01-25 18:16:50 +0000

    ch12 completed

--- ch11.py
+++ ch11.py
@@ -2,20 +2,10 @@ from Tools import Binary, Crypto
 import random
 
 def encryptionOracle(data):
-    print('Plaintext: {}'.format(data.toHex()))
     randomKey = Binary.random(8*16)
-
-    print('Key:       {}'.format(randomKey.toHex()))
-    
     addBefore = Binary.random(random.randrange(5,11)*8)
     addAfter = Binary.random(random.randrange(5,11)*8)
-
-    print('Before:    {}\nAfter:     {}'.format(addBefore.toHex(), addAfter.toHex()))
-
     data = addBefore + data + addAfter
-
-    print('Plaintext: {}'.format(data.toHex()))
-
     IV = Binary.random(8*16)
 
     c = Crypto(data)
@@ -24,13 +14,12 @@ def encryptionOracle(data):
         c.encAES_ECB(randomKey)
         mode = 'ECB'
     else:
-        print('IV:        {}'.format(IV.toHex()))
         c.encAES_CBC(randomKey, IV)
         mode = 'CBC'
 
     return (mode, c.data)
 
-def detectECB(cipher):
+def detectMode(cipher):
     blocks = [Binary(cipher[x:x+8*16]) for x in range(0, len(cipher), 8*16)]
     repeated = 0
 
@@ -46,14 +35,22 @@ def detectECB(cipher):
         i += 1
 
     if not repeated:
-        return False
-    return True
+        return 'CBC'
+    return 'ECB'
 
 data = Binary('A'*16*10, 128)
+nTests = 25
+count = 0
 
-cipher = encryptionOracle(data)
-print('Mode:      {}\nCipher:    {}'.format(cipher[0], cipher[1].toHex()))
-if detectECB(cipher[1]):
-    print('====ECB detected====')
-else:
-    print('====CBC detected=====')
+for i in range(nTests):
+    cipher = encryptionOracle(data)
+    passed = False
+    detectedMode = detectMode(cipher[1])
+
+    if detectedMode == cipher[0]:
+        passed = True
+        count += 1
+
+    print('({:>2}/{}) Used: {} Detected: {} --> Passed: {}'.format(i+1, nTests, cipher[0], detectedMode, passed))
+
+print('Passed {:.0f}% of tests'.format(100*float(count) / nTests))
---
+++ ch12.py
@@ -0,0 +1,88 @@
+from Tools import Binary, Crypto
+import random
+
+# constant random key
+randomKey = Binary.random(8*16)
+
+def encryptionOracle(data):
+    extraData = Binary('Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK', 64)
+    data = data + extraData
+
+    c = Crypto(data)
+
+    c.encAES_ECB(randomKey)
+    return c.data
+
+def getBlockSize(oracle):
+    startCounting = -1
+    extraBytes = 1
+    baseLength = len(oracle(Binary()))
+
+    while 1:
+        newLength = len(oracle(Binary('A'*extraBytes, 128)))
+
+        if newLength > baseLength and startCounting == -1:
+            startCounting = extraBytes
+        
+        elif newLength > baseLength:
+            return newLength - baseLength
+
+        extraBytes += 1
+
+def detectMode(oracle):
+    cipher = oracle(Binary('A'*16*10, 128))
+    blocks = [Binary(cipher[x:x+8*16]) for x in range(0, len(cipher), 8*16)]
+    repeated = 0
+
+    i = 0
+    while i < len(blocks) - 1:
+        ii = i + 1
+        while ii < len(blocks):
+            if blocks[i] == blocks[ii]:
+                blocks = blocks[:ii] + blocks[ii+1:len(blocks)]
+                repeated += 1
+                ii -= 1
+            ii += 1
+        i += 1
+
+    if not repeated:
+        return 'CBC'
+    return 'ECB'
+
+def decECB(oracle):
+    blockSize = getBlockSize(oracle)
+    nblocks = int(len(oracle(Binary())) / blockSize)
+    
+    if detectMode(oracle) != 'ECB':
+        print('Not ECB. Exiting')
+        return
+
+    plainText = ''
+
+    wordCiphers = []
+    for wordSize in range(int(blockSize / 8) - 1, -1, -1):
+        word = Binary('A' * wordSize, 128)
+        wordCiphers.append((word, wordSize, oracle(word)))
+
+    for block in range(nblocks):
+        for cipher in wordCiphers:
+            blockCipher = cipher[2][block * blockSize:block * blockSize + blockSize]
+            known = cipher[0] + Binary(plainText, 128)
+
+            found = False
+            for newChar in range(127, -1, -1):
+                cipherWithNewChar = oracle(known + Binary(chr(newChar), 128))
+                if blockCipher == cipherWithNewChar[block * blockSize:block * blockSize + blockSize]:
+                    plainText += chr(newChar)
+                    print('Block {:>2} Digit {:>2}: {}'.format(block+1, int(blockSize / 8 - cipher[1]), chr(newChar)))
+                    found = True
+                    break
+            
+            if not found and block + 1 < nblocks:
+                print('New char not found. Exiting')
+                exit()
+            else:
+                return plainText
+
+print(decECB(encryptionOracle))
+
